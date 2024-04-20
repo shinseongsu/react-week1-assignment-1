31c40d0d16f0ed6463a1ecde64dad5c4f629cbad
Shin seongsu <tlstjdtn321@naver.com>
2024-04-21 01:42:50 +0900

    feat: 일반적인 자바스크립트 사용방법

---
+++ src/study.js
@@ -0,0 +1,5 @@
+const app = document.getElementById('app');
+const paragraph = document.createElement('div');
+const text = document.createTextNode('텍스트입니다.');
+paragraph.appendChild(text);
+app.appendChild(paragraph);
